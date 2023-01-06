0c418f3d9636d9515150665f16e4eb687a3b75b6
Irina Pletneva <98366088+pletniri@users.noreply.github.com>
2023-01-06 17:19:34 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,21 +6,38 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
 
     int counter = 1;
-    double sum = 0;
+//    double sum = 0;
+    double min = 0;
+    double max = 0;
 
-    while(counter <= 5){
+
+    while(counter > 0){
         System.out.println("Enter number #" + counter + ":");
         String nextNumber = scanner.nextLine();
         try{
 //            int number = Integer.parseInt(nextNumber);
             double number = Double.parseDouble(nextNumber);
+            if(number > max){
+                max = number;
+            } else if((number < min && counter > 1) ||
+                    (counter == 1)) {
+                min = number;
+            }
+
             counter++;
-            sum +=number;
+
         } catch (NumberFormatException nfe) {
-            System.out.println("Invalid Number");
+//            System.out.println("Invalid Number");
+            break;
         }
 
     }
-        System.out.println(sum);
+        if(counter > 1) {
+            System.out.println("The minimum number entered is: " + min);
+            System.out.println("The maximum number entered is: " + max);
+        } else {
+            System.out.println("No valid data entered");
+        }
+
     }
 }
